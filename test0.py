be95167bed991ffcb9c522cd6c0c3f5cf4367f01
AndySag <andysag@gmail.com>
2021-02-17 20:17:25 +0300

    Работа с проектом

--- test0.py
+++ test0.py
@@ -24,29 +24,45 @@
 #             print(res)
 #             ser.close()
 
+import time
 import serial
+
 
 hex_string = '55 55 00 00 AA'
 ser_message = bytes.fromhex(hex_string)
 
 ser = serial.Serial(
-    port='COM4', 
-    baudrate=9600, 
-    bytesize=EIGHTBITS,
-    parity=PARITY_NONE,
-    stopbits=STOPBITS_ONE,
-    timeout = 0)
+    port='COM3', 
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout = 0.1)
     
-ser.close()    
+ser.close()  
+
 ser.open()
-ser.flushInput()
+ser.write(ser_message)
+ser_message_read = ser.readline()
+print(ser_message_read.hex())
+
 while True:
     try:
+        
         ser.write(ser_message)
         ser_message_read = ser.readline()
-        time.sleep(0.5)
+        # if len(ser_message_read) == 0:
+        #     ser_message_read = 0
+
+        # 5555000e02000001e31f001201f40100790240
+        # 5555000e020000013c1f001201f30100790298
+        # 5555000e02000001041f001201f40100790261
+        # 5555000e02000001b020001201f4010079020e
+        # 5555000e020000012521001201f40100790284
+
+        
 
         print(ser_message_read.hex())
+        time.sleep(0.5)
     except:
         print("Interrupt")
         break
