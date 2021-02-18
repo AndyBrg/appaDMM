fa3083a2ce1df174435cc1d0f34169d04a5c4bd8
AndySag <andysag@gmail.com>
2021-02-18 20:09:02 +0300

    Работа с проектом

--- test0.py
+++ test0.py
@@ -35,13 +35,16 @@ import threading
 # 5555000e02000001b020001201f4010079020e
 # 5555000e020000012521001201f40100790284
 
+# b'UU\x00\x0e\x01\x00\x00\x00\x19\x04\x00\x0c\x01\x00\x00\x00y\x02^' 19
+
 def send_port():
-    threading.Timer(0.5, send_port).start()
+    # threading.Timer(0.5, send_port).start()
     ser.write(ser_message)
     ser_message_read = ser.readline()
-
-    print(ser_message_read)
+   
+    print(ser_message_read, len(ser_message_read))
     # print(ser_message_read.hex())
+    time.sleep(0.5)
 
 
 hex_string = '55 55 00 00 AA'
@@ -52,17 +55,16 @@ ser = serial.Serial(
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    timeout = 0.1)
+    timeout = 0)
     
 ser.close()  
 
 ser.open()
-if ser.is_open:
-    while True:
-        try:
-            send_port()
-        except Exception as exc:
-            print('type: {0}, message: {1}'.format(type(exc), str(exc)))
-            break
-    else:
-        ser.close()
+
+while True:
+    try:
+        send_port()
+    except Exception as exc:
+        print('type: {0}, message: {1}'.format(type(exc), str(exc)))
+        break
+
