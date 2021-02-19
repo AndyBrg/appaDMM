67e6cd1eaee5dba0a616fc38a92c4b30e27810eb
AndySag <andysag@gmail.com>
2021-02-19 19:45:18 +0300

    Добавлен файл

---
+++ check_crc.py
@@ -0,0 +1,17 @@
+
+def int_to_bytes(number: int) -> bytes:
+    return number.to_bytes(
+        length=(8 + (number + (number < 0)).bit_length()) // 8,
+        byteorder='big', signed=True)
+
+def check_crc(data: bytes, package_len: int) -> bool:
+    if len(data) == package_len:
+        check_sum = 0
+        for byte in range(package_len - 1):
+            check_sum += data[byte]
+        if data[-1:] == int_to_bytes(check_sum)[-1:]:
+            return True
+        else:
+            return False
+    else:
+        return False
