3ee5751b318246ee996abf3f13a9daaf069b1375
AndySag <andysag@gmail.com>
2021-02-20 12:04:35 +0300

    Убрана длина пакета из параметров

--- check_crc.py
+++ check_crc.py
@@ -4,7 +4,8 @@ def int_to_bytes(number: int) -> bytes:
         length=(8 + (number + (number < 0)).bit_length()) // 8,
         byteorder='big', signed=True)
 
-def check_crc(data: bytes, package_len: int) -> bool:
+def check_crc(data: bytes) -> bool:
+    package_len = 19 #Длина пакета 
     if len(data) == package_len:
         check_sum = 0
         for byte in range(package_len - 1):
