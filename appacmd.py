20f3ab628ff25bc055da3ad67bab517f7811fc8d
AndySag <andysag@gmail.com>
2021-02-20 14:11:55 +0300

    Работа с проектом

--- appacmd.py
+++ appacmd.py
@@ -27,6 +27,8 @@
 import time
 import serial
 # import threading
+from ctypes import *
+
 from datetime import datetime
 
 from check_crc import check_crc
@@ -37,31 +39,37 @@ from check_crc import check_crc
 # Not connected...
 # Please connect the device
 
+class pxtup(Structure):
+    _fields_ = [('red', c_int),
+                ('green', c_int),
+                ('blue', c_int)]
+
 p_time = 0.5
+index = 0
 
 def send_port(poll_time: float):
     # threading.Timer(0.5, send_port).start()
-    ser.write(ser_message)
+    ser.write(send_message)
     time.sleep(poll_time)
     time = time_tmp = datetime.isoformat(
         datetime.now(), sep=' ', timespec='milliseconds')
-    ser_message_read = ser.readline()
-    if check_crc(ser_message_read):
+    receive_message = ser.readline()
+    if check_crc(receive_message):
         crc = "OK"
-        index = index + 1
+        index += 1
     else:
         crc ="ER"
 
-    print(cnt, time, ser_message_read.hex(), crc)
+    print(cnt, time, receive_message.hex(), crc)
 
 
 
 
-hex_string = '55 55 00 00 AA'
-ser_message = bytes.fromhex(hex_string)
+com_appa = "55 55 00 00 AA"
+send_message = bytes.fromhex(com_appa)
 
 ser = serial.Serial(
-    port='COM3', 
+    port="COM3", 
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
@@ -76,6 +84,6 @@ while True:
         if ser.is_open:
             send_port(p_time)
     except Exception as exc:
-        print('type: {0}, message: {1}'.format(type(exc), str(exc)))
+        print("type: {0}, message: {1}".format(type(exc), str(exc)))
         break
 
