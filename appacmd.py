6a17a3b68ac0f5750cfab43f59e4e385ca5e7bd7
AndySag <andysag@gmail.com>
2021-02-21 20:35:21 +0300

    Работа с проектом

--- appacmd.py
+++ appacmd.py
@@ -1,6 +1,6 @@
 # from serial import Serial
 
-# def send_message(v):
+# def data_send(v):
 #     if v == 0:
 #         hex_string = '55 55 00 00 AA'
 #     elif v == 1:
@@ -26,12 +26,12 @@
 
 import time
 import serial
-# import threading
-from ctypes import *
 
+from ctypes import c_int
+from threading import Timer
 from datetime import datetime
-
 from check_crc import check_crc
+
 
 # 5555000e02000001e31f001201f40100790240
 # b'UU\x00\x0e\x01\x00\x00\x00\x19\x04\x00\x0c\x01\x00\x00\x00y\x02^' 19
@@ -39,51 +39,94 @@ from check_crc import check_crc
 # Not connected...
 # Please connect the device
 
-class pxtup(Structure):
-    _fields_ = [('red', c_int),
-                ('green', c_int),
-                ('blue', c_int)]
+# class pxtup(Structure):
+#     _fields_ = [('red', c_int),
+#                 ('green', c_int),
+#                 ('blue', c_int)]
 
-p_time = 0.5
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+# rt = RepeatedTimer(1, hello, "World")
+
+poll_time = 0.5
+
+
 index = 0
 
-def send_port(poll_time: float):
-    # threading.Timer(0.5, send_port).start()
-    ser.write(send_message)
-    time.sleep(poll_time)
-    time = time_tmp = datetime.isoformat(
-        datetime.now(), sep=' ', timespec='milliseconds')
-    receive_message = ser.readline()
-    if check_crc(receive_message):
+global time_receive
+global data_receive
+
+def send_port():
+    # threading.Timer(poll_time, send_port).start()
+    ser.write(data_send)
+    time.sleep(0.5)
+    data_receive = ser.readline()
+    time_receive = datetime.isoformat(
+        datetime.now(), sep=' ', timespec='milliseconds')    
+    if check_crc(data_receive):
         crc = "OK"
+        global index
         index += 1
     else:
         crc ="ER"
 
-    print(cnt, time, receive_message.hex(), crc)
+    print(index, time_receive, crc, data_receive.hex())
 
 
 
 
-com_appa = "55 55 00 00 AA"
-send_message = bytes.fromhex(com_appa)
+appa_109n = "55 55 00 00 AA"
+data_send = bytes.fromhex(appa_109n)
 
 ser = serial.Serial(
     port="COM3", 
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    timeout = 0)
+    xonxoff=False,
+    timeout=0)
     
 ser.close()  
 
 ser.open()
 
-while True:
-    try:
-        if ser.is_open:
-            send_port(p_time)
-    except Exception as exc:
-        print("type: {0}, message: {1}".format(type(exc), str(exc)))
-        break
+# while True:
+#     try:
+#         if ser.is_open:
+#             send_port(p_time)
+#     except Exception as exc:
+#         print("type: {0}, message: {1}".format(type(exc), str(exc)))
+#         break
 
+if ser.is_open:
+    send_port()
+    rt = RepeatedTimer(poll_time, send_port)
+
+
+try:
+    time.sleep(10) # your long-running job goes here...
+finally:
+    rt.stop() # better in a try/finally block to make sure the program ends!
