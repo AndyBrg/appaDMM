a2e97fc1713135ad0f7f2f73d9742968c69d1939
AndySag <andysag@gmail.com>
2021-02-26 15:29:48 +0300

    Работа с проектом

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ from appa_109n import *
 # tmp = b'UU\x00\x0e\x04\x00\x00\x80I\x00\x00\x0b\x01\x00\x00\x00\x00\x00\x91'
 #tmp = b'UU\x00\x0e\x01\x01\x00\x01{\x0c\x00\x0b\x01\x00\x00\x00\x00\x00N'
 # tmp = b'UU\x00\x0e\x04\x01\x00\x80\xf5\x03\x00Q\x01\x00\x00\x00\x00\x00\x87'
-tmp = b'UU\x00\x0e\x04\x01\x00\x80\xd0\x07\x00Q\x01\x00\x00\x00\x00\x00f'
+# tmp = b'UU\x00\x0e\x04\x01\x00\x80\xd0\x07\x00Q\x01\x00\x00\x00\x00\x00f'
 
 # tmp = b'UU\x00\x0e\x03\x01\x00\x00\xbf\x00\x00\\\x01\x00\x00\x00\x00\x00\xd8'
 # 10 2021-02-25 20:04:53.024 4.8896 KΩ
-# tmp = b'UU\x00\x0e\x03\x00\x00\x00\xa2\x07\x00R\x01\x00\x00\x00\x00\x00\xb7'
+tmp = b'UU\x00\x0e\x03\x00\x00\x00\xa2\x07\x00R\x01\x00\x00\x00\x00\x00\xb7'
 # 10 2021-02-25 20:06:43.003 5002.24 Ω
 
 # tmp = b'UU\x00\x0e\x04\x01\x00\x80\xf5\x03\x00Q\x01\x00\x00\x00\x00\x00\x87'
@@ -45,7 +45,7 @@ if appa.hex() == "5555000e":
 elif appa.hex() == "55550029":
     print("APPA 107N 105N")
 
-print("Знак градуса \u00B0С")
+# print("Знак градуса \u00B0С")
 print()
 
 # print(tmp[4])
@@ -72,7 +72,7 @@ main_read = int.from_bytes(tmp[8:9] + tmp[9:10] + tmp[10:11], byteorder = "littl
 
 # print("MAIN", main_read.hex())
 # main_read = int.from_bytes(main_read, byteorder = "little")
-print("Main value=", main_read)
+print("Main read=", main_read)
 
 main_status_bits = bin(tmp[11])[2:].zfill(8)
 
@@ -95,7 +95,7 @@ sub_read = int.from_bytes(tmp[13:14] + tmp[14:15] + tmp[15:16], byteorder = "lit
 # print("SUB", sub_read.hex())
 # sub_read_b = int.from_bytes(sub_read, byteorder = "big")
 # sub_read_l = int.from_bytes(sub_read, byteorder = "little")
-print("Sub value=", sub_read)
+print("Sub read=", sub_read)
 
 sub_status_bits = bin(tmp[16])[2:].zfill(8)
 
